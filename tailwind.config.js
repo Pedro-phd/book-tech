489ca8b2728561891cca80204c1b283e72259419
ArSaN <arthnardini24@gmail.com>
2021-08-31 20:48:41 -0300

    Add global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgroundPrimary: "#04001B",
+        secondary: "#FF005C",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
